4155a097e02bc3745709ca14abdf02500cb68d52
Juan Esteban Florez Coronel <juan.florez@upr.edu>
2023-10-06 02:49:59 -0400

    change

--- PI_DA/pages/01_EDA.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-if st.checkbox('show text'):
-    st.write('hola')
--- PI_DA/pages/02_rawData.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-df = pd.read_csv('data/df_homicides.csv')
-
-if st.checkbox('show data'):
-    st.dataframe(df)
-
-
